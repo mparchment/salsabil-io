b2eb09dd5cba9b720b8163b2ae09d4f98e58504a
Michael Parchment <85087548+mparchment@users.noreply.github.com>
2024-09-01 14:50:48 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Salsabil';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Salsabil is a blog dedicated to the study of Islamic sciences, with a particular focus on the Hanbali school. The primary aim is to provide in-depth analysis and discussions on Hanbali jurisprudence and theology, while offering a comprehensive resource for an English-speaking audience.';
